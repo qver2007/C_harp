c4860bf8cf97426d4d8dff45e2f55745c74b1946
unknown <Pushkin@example.com>
2022-07-08 11:58:53 +0300

    talk1.1.2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+Console.WriteLine("Да");
+else 
+Console.WriteLine("Нет");
+
